6a8453c9aa959fd3fd7a9ad6b9d6cc2bc9504746
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-09-05 12:27:16 +0100

    Set up models, flask-migrate and normalize line endings in migration files

--- poultry_manager/__init__.py
+++ poultry_manager/__init__.py
@@ -1,17 +1,30 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from .config import Config
 from .routes import bp
+
+# Instantiate the extensions without an app
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # Initialize the extensions with the app
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+
     # import routes and models
     from . import routes
     from . import models
 
-    # register routes
+    # register the blueprint for routes
     app.register_blueprint(bp)
 
     return app
--- poultry_manager/static/scripts/home.js
+++ poultry_manager/static/scripts/home.js
@@ -1,14 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Smooth Scroll for Navigation Links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
-    });
-
     // Animated Scroll Effects
     const faders = document.querySelectorAll('.fade-in');
     const appearOptions = {
